10b1c2753e8eafef296ebf74b2b77aad07743f8f
TomyNilson <tomynilso@gmail.com>
2026-08-22 14:50:21 +0000

    Allow dynamic product page rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { CategoryMenu, Hero, IntroducingSection, ProductsSection, PromotionWidget, ReviewsSection } from "@/components";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return (
     <>
