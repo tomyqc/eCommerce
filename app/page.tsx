139b5a4ac2e9b16023c7962e31df5058397c7b96
TomyNilson <tomynilso@gmail.com>
2026-08-22 14:57:37 +0000

    Show catalog slider on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { CategoryMenu, Hero, IntroducingSection, ProductsSection, PromotionWidget, ReviewsSection } from "@/components";
+import { CategoryMenu, Hero, IntroducingSection, ProductsSection, PromotionWidget, ReviewsSection, SimpleSlider } from "@/components";
 
 export const dynamic = "force-dynamic";
 
@@ -10,6 +10,7 @@ export default function Home() {
     <PromotionWidget />
     <CategoryMenu />
     <ProductsSection />
+    <SimpleSlider />
     <ReviewsSection />
     </>
   );
